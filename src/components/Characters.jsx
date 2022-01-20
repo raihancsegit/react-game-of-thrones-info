bab58b692cfbe1c0026228e9b96ac7bb40f199a4
Raihan <raihanislam.cse@gmail.com>
2022-01-20 22:28:14 +0600

    update

---
+++ src/components/Characters.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { Box, Grid } from '@material-ui/core';
+import { data }  from '../constants/data';
+import Character from './Character';
+
+const Characters = () => {
+    console.log(data.characters);
+    return (
+        <Box style={{margin: '10px 35px'}}>
+            <Grid container>
+                {
+                    data.characters.map((value, index) => (
+                        <Grid item key={index}><Character key={value.id} value={value} /></Grid>
+                    ))
+                }   
+            </Grid>
+        </Box>
+    )
+}
+
+export default Characters
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AppBar, Toolbar, makeStyles } from '@material-ui/core';
+import logo from '../images/logo.jpg';
+
+const useStyle = makeStyles({
+    header: {
+        background: '#000000'
+    },
+    logo: {
+        width: 250
+    }
+});
+ const Header  = () => {
+    const classes = useStyle()
+    return (
+        <AppBar className={classes.header} position="static">
+            <Toolbar>
+                <img className={classes.logo} src={logo} alt="got" />
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header;
